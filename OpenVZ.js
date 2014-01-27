0edcced977ed1a9613a8af42f6594892bd4f236c
joedulin <joedulin@gmail.com>
2014-01-27 15:57:44 -0700

    Oopses from a bad copy/paste

--- OpenVZ.js
+++ OpenVZ.js
@@ -3,8 +3,7 @@
   var Container, OpenVZ, Validator, async, colors, exec, fs, nexpect,
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
     __hasProp = {}.hasOwnProperty,
-    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prot
-otype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
                                                                                                                                                                                                            
   fs = require('fs');
                                                                                                                                                                                                            
